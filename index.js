961510270ae95c6a6ac742a3e75477ac5af51204
Mark <mark.njau@student.moringaschool.com>
2024-07-08 12:14:24 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function(){
+    // select the paragraph element with id "text"
+    const pararaph = document.getElementById('text');
+
+    // Change its text content
+    pararaph.textContent = "This is really cool!";
+});
+  
